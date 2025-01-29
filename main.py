036ec9916672043129114f874c1eabba053089ac
MikeMarinaro-Render <129456836+MikeMarinaro-Render@users.noreply.github.com>
2025-01-29 16:56:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    if("URL" in os.environ)
+    if("URL" in os.environ):
         print("URL key exists! ")
-else:
+    else:
         print("URL key DOES NOT exist ")
     return {"message": "Hello World"}
 
